9f01b4e4901b3ee5f33ebfbeedc6f9872ed46cf3
Richard Ottinger <richard.ottinger@gmail.com>
2023-01-24 06:29:32 -0600

    add public component and do not create test file when adding a new component or service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent
   ],
   imports: [
     BrowserModule,
